1efe8217752df26b01481165de7d18935da2be9f
yechanpark <qkrdpcks0419@naver.com>
2019-04-05 21:02:21 +0900

    first commit

---
+++ src/main/java/teemoDevs/MafiaWebSocket/MafiaWebSocketApplication.java
@@ -0,0 +1,13 @@
+package teemoDevs.MafiaWebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teemoDevs/MafiaWebSocket/MafiaWebSocketApplicationTests.java
@@ -0,0 +1,16 @@
+package teemoDevs.MafiaWebSocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MafiaWebSocketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
